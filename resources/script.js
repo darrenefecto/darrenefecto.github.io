235a8473ceb2996abd940555eebf2b1832b8c3ab
darrenefecto <diefaust132@gmail.com>
2022-12-29 17:29:28 +0100

    Website update 29.12.2022
    Just normal Website instead of the version before

---
+++ resources/script.js
@@ -0,0 +1,10 @@
+function detectDevice() {
+    if ((window.innerWidth <= 800) && (window.innerHeight <= 600))
+    {
+        console.log("no Computer detected!")
+    }
+    else
+    {
+        console.log("Computer detected!")
+    }
+  }
