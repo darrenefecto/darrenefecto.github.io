4a8ed0e6f5020fe1b7e76acd84c18329ee645caf
darrenefecto <diefaust132@gmail.com>
2023-05-28 14:15:54 +0200

    website updated

--- script.js
+++ script.js
@@ -8,36 +8,48 @@ function MobileMenu() {
     {
         x.style.display = "block";
     }
+
+
+}
+
+function mobileDetect() {
+  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    var rainbowButton = document.getElementById('codepen-button');
+    if (!rainbowButton) return;
+    rainbowButton.style.setProperty('--animationPlayState', 'running');
+  }else{
+    var rainbowButton = document.getElementById('codepen-button');
+    if (!rainbowButton) return;
+    rainbowButton.style.setProperty('--animationPlayState', 'paused');
+  }
 }
 
 function transfer(site)
 {   
-    if (site)
-      window.location.href = "more.html";
-    
-    if (!site)
-      window.location.href = "index.html";
+  if (site)
+    window.location.href = "more.html";
+  
+  if (!site)
+    window.location.href = "index.html";
 
 }
 
 function cacheData() {
-    var currentUrl = window.location.href;
-    const cacheName = "myCache";
-    caches.open(cacheName)
-      .then(cache => {
-        cache.match(currentUrl)
-          .then(response => {
-            if (response) {
-              return response;
-            } else {
-              return fetch(currentUrl)
-                .then(response => {
-                  cache.put(currentUrl, response);
-                  return response;
-                });
-            }
-          });
-      });
+  var currentUrl = window.location.href;
+  const cacheName = "myCache";
+  caches.open(cacheName)
+    .then(cache => {
+      cache.match(currentUrl)
+        .then(response => {
+          if (response) {
+            return response;
+          } else {
+            return fetch(currentUrl)
+              .then(response => {
+                cache.put(currentUrl, response);
+                return response;
+              });
+          }
+        });
+    });
   }
-  
-  
